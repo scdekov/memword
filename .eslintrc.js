0d00f929b0b2020d46d25bca2c76a4f2b0df6e7f
Svetoslav Dekov <svetlio1994@gmail.com>
2018-09-09 21:58:17 +0300

    allow debugger in JS

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "rules": {
         "indent": ["error", 4],
         "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false  }],
+        "no-debugger": "off"
     },
     "globals": {
         "fetch": false
