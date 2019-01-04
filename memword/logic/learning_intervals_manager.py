c41541404873758abdb765c8f432cc321ba0b00f
Svetoslav Dekov <svetlio1994@gmail.com>
2019-01-04 10:37:06 +0200

    Remove usage of target difficulties for adjusting intervals

--- memword/logic/learning_intervals_manager.py
+++ memword/logic/learning_intervals_manager.py
@@ -8,9 +8,9 @@ class LearningIntervalsManager:
 
     @classmethod
     def handle_submitted_question(cls, question):
-        difficulty = cls._adjust_question_difficulty(question)
-        interval_change = cls._get_interval_change(question, difficulty)
+        cls._adjust_question_difficulty(question)
 
+        interval_change = cls._get_interval_change(question)
         user_intervals = UserLearningIntervals.objects.get(user=question.target.author)
         same_target_questions_count = Question.objects.filter(target=question.target,
                                                               passed=True,
@@ -22,8 +22,8 @@ class LearningIntervalsManager:
         # may be we should increase all intervals > n ?
 
     @classmethod
-    def _get_interval_change(cls, question, difficulty):
-        base_change_percent = abs(cls.CONFIDENCE_LEVEL_TARGET - question.confidence_level) * difficulty.difficulty
+    def _get_interval_change(cls, question):
+        base_change_percent = abs(cls.CONFIDENCE_LEVEL_TARGET - question.confidence_level)
         if question.confidence_level > cls.CONFIDENCE_LEVEL_TARGET:
             return base_change_percent
         else:
@@ -41,4 +41,3 @@ class LearningIntervalsManager:
 
         difficulty.difficulty = max(difficulty.difficulty + change_amount, difficulty.BASE_DIFFICULTY)
         difficulty.save()
-        return difficulty
