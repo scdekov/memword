ecc0a1098db3243513002663bbb7bc9a73321144
Svetoslav Dekov <svetlio1994@gmail.com>
2019-01-07 16:13:47 +0200

    Config prod database

--- server/settings.py
+++ server/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
 
 try:
     from server import credentials
@@ -93,12 +95,14 @@ WSGI_APPLICATION = 'memword.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+if ENVIRONMENT == 'production':
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
 
 
 # Password validation
