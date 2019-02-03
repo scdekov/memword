42a371d318cda4da5ebae6e698e1209876c24be3
Svetoslav Dekov <svetlio1994@gmail.com>
2019-02-03 14:25:46 +0200

    Enable authentication

--- server/settings.py
+++ server/settings.py
@@ -65,9 +65,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'memword.middleware.OneUserAuthentication',  # I want to always be logged as me for now
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -168,7 +167,7 @@ IMAGE_DEPOT_URL = 'https://image-depot.herokuapp.com/api/links'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'memword.api.CsrfExemptSessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
